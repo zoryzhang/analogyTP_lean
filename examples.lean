c043ae7705311c898a07e6ab59cbfdc7da84653b
zoryzhang <hereiszory@gmail.com>
2024-02-27 21:53:49 -0600

    add sorry back

--- examples.lean
+++ examples.lean
@@ -1,4 +1,4 @@
---import Mathlib
+import Mathlib
 --import LeanCopilot
 --import Aesop
 
@@ -22,24 +22,25 @@ theorem foo (a : Nat) : a + 1 = Nat.succ a := by rfl
 
 --def f (n : ℕ) : ℕ := n ^ 3 - n
 
---example (n : ℕ ) : 2 ^ n > n := by
---  have h (m : ℕ ) : (2 ^ m) > m → (2 ^ (m+1)) > (m+1) := by
---    intro h
---    rw [pow_succ]
---    linarith
---  induction n with
---  | zero => simp
---  | succ n ih => exact h n ih
+theorem ex2_source (n : ℕ ) : 2 ^ n > n := by
+  have h (m : ℕ ) : (2 ^ m) > m → (2 ^ (m+1)) > (m+1) := by
+    intro h
+    rw [pow_succ]
+    linarith
+  induction n with
+  | zero => simp
+  | succ n ih => exact h n ih
 
---example (n : ℕ ) : 3 ∣ n ^ 3 - n := by
---  have h (m : ℕ ) : 3 ∣ m ^ 3 - m → 3 ∣ (m + 1) ^ 3 - (m + 1) := by
---    intro h2
---    ring_nf
---    obtain ⟨k, hk⟩ := h2
---    use m + m ^ 2 + k
---    ring_nf
---    rw [Nat.mul_comm k 3, ←hk, Nat.sub_add_eq]
---    ring_nf
---  induction n with
---  | zero => simp [Nat.zero_eq, zero_add, forall_const]
---  | succ n ih => exact h n ih
+theorem ex2_target (n : ℕ ) : 3 ∣ n ^ 3 - n := by
+  have h (m : ℕ ) : 3 ∣ m ^ 3 - m → 3 ∣ (m + 1) ^ 3 - (m + 1) := by
+    intro h2
+    ring_nf
+    obtain ⟨k, hk⟩ := h2
+    use m + m ^ 2 + k
+    ring_nf
+    rw [Nat.mul_comm k 3, ←hk, Nat.sub_add_eq]
+    ring_nf
+    sorry
+  induction n with
+  | zero => simp [Nat.zero_eq, zero_add, forall_const]
+  | succ n ih => exact h n ih
