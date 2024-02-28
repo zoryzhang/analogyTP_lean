e0a7a4a7e028137538d24e37c71722d9496fc31c
zoryzhang <hereiszory@gmail.com>
2024-02-28 15:11:29 -0600

    delete manifest

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ package «examples» {
 }
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "stable"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.6.0-rc1"
 
 @[default_target]
 lean_lib «examples» {
--- test.py
+++
@@ -1,4 +0,0 @@
-from lean_dojo import LeanGitRepo, trace
-
-repo = LeanGitRepo("https://github.com/zoryzhang/analogyTP_lean", "d63e95f137c10b934910c01ea9dada488031b49f")
-trace(repo, dst_dir="traced_lean4-example")
