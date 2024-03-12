44c563a8202430a2c7e42ce01e0c5566d3624a3c
subfish-zhou <91206366+subfish-zhou@users.noreply.github.com>
2024-03-12 17:27:19 +0100

    clean

--- lakefile.lean
+++ lakefile.lean
@@ -3,15 +3,10 @@ open Lake DSL
 
 package «examples» {
   -- add any package configuration options here
-  --moreLinkArgs := #["-L./.lake/packages/LeanCopilot/.lake/build/lib", "-lctranslate2"]
 }
-
---require mathlib from git
---  "https://github.com/leanprover-community/mathlib4.git" @ "v4.6.0-rc1"
 
 @[default_target]
 lean_lib «examples» {
   -- add any library configuration options here
 }
 
---require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.1.1"
