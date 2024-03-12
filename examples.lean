e629e83c2a1c231946bca41b6af7c7c31dab8047
subfish-zhou <91206366+subfish-zhou@users.noreply.github.com>
2024-03-12 22:23:31 +0100

    test theorem

--- examples.lean
+++ examples.lean
@@ -1,4 +1,18 @@
-variable {p : Prop}
-variable {q : Prop}
+theorem t1 (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h
+  . apply Or.inr
+    assumption
+  . apply Or.inl
+    assumption
 
-theorem t1 : p → q → p := fun hp : p => fun hq : q => hp
+theorem t2 (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
+  apply Iff.intro
+  . intro h
+    match h with
+    | ⟨_, Or.inl _⟩ => apply Or.inl; constructor <;> assumption
+    | ⟨_, Or.inr _⟩ => apply Or.inr; constructor <;> assumption
+  . intro h
+    match h with
+    | Or.inl ⟨hp, hq⟩ => constructor; exact hp; apply Or.inl; exact hq
+    | Or.inr ⟨hp, hr⟩ => constructor; exact hp; apply Or.inr; exact hr
