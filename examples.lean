964b57df4906c0bd94cb38998f7241a2d6ce652f
zoryzhang <hereiszory@gmail.com>
2024-05-12 17:01:45 -0400

    add duper example

--- examples.lean
+++ examples.lean
@@ -1,4 +1,5 @@
 import Mathlib
+--import LeanCopilot
 
 theorem ex2_src (n : ℕ ) : 2 ^ n > n := by
   have h (m : ℕ ) : (2 ^ m) > m → (2 ^ (m+1)) > (m+1) := by
@@ -41,3 +42,26 @@ theorem t2 (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
     match h with
     | Or.inl ⟨hp, hq⟩ => constructor; exact hp; apply Or.inl; exact hq
     | Or.inr ⟨hp, hr⟩ => constructor; exact hp; apply Or.inr; exact hr
+
+theorem duper {x : ℝ } (h: x ^ 2 = 1) : x = 1 ∨ x = -1 := by
+  --proof 1
+  --exact sq_eq_one_iff.mp h
+
+  --proof 2
+  --simp_all only [sq_eq_one_iff]
+
+  --proof 3
+  --simpa using  h
+
+  --proof 4
+  have h' : x ^ 2 - 1 = (x+1) * (x-1) := by ring
+  have h'' : x ^ 2 - 1 = 0 := by rw [h, sub_self]
+  have h''' : (x+1) * (x-1) = 0 := by rw [←h', h'']
+  have h'''' : x + 1 = 0 ∨ x - 1 = 0 := by
+    apply eq_zero_or_eq_zero_of_mul_eq_zero
+    assumption
+  cases h''''
+  . right
+    linarith
+  . left
+    linarith
--- lakefile.lean
+++ lakefile.lean
@@ -3,12 +3,17 @@ open Lake DSL
 
 package «examples» {
   -- add any package configuration options here
+  --moreLinkArgs := #[
+  --  "-L./.lake/packages/LeanCopilot/.lake/build/lib",
+  --  "-lctranslate2"
+  --]
 }
 
 @[default_target]
 lean_lib «examples» {
   -- add any library configuration options here
 }
+--require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.1.1"
 
 require mathlib from git
     "https://github.com/leanprover-community/mathlib4" @ "v4.6.0-rc1"
